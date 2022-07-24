795f8e675248191f8d0ee25bec19e43cacb95fca
Karthik <aceventura18@gmail.com>
2022-07-24 11:20:56 +0800

    Initial Project setup

---
+++ src/main/java/com/karthik/jpm/marsrover/MarsRoverApplication.java
@@ -0,0 +1,13 @@
+package com.karthik.jpm.marsrover;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MarsRoverApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MarsRoverApplication.class, args);
+	}
+
+}
