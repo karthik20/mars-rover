d4248f3b5a8c6af0fbb491946c73e630c1255edc
Karthik <aceventura18@gmail.com>
2022-07-24 23:30:49 +0800

    Update code base for 1.8 to replace List.of() from jdk 9

--- src/main/java/com/karthik/jpm/marsrover/command/CommandExecutor.java
+++ src/main/java/com/karthik/jpm/marsrover/command/CommandExecutor.java
@@ -2,7 +2,6 @@ package com.karthik.jpm.marsrover.command;
 
 import java.util.ArrayList;
 import java.util.HashMap;
-import java.util.List;
 import java.util.Map;
 import java.util.Map.Entry;
 import java.util.function.Function;
--- src/test/java/com/karthik/jpm/marsrover/command/CommandTest.java
+++ src/test/java/com/karthik/jpm/marsrover/command/CommandTest.java
@@ -3,6 +3,7 @@ package com.karthik.jpm.marsrover.command;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertThrows;
 
+import java.util.ArrayList;
 import java.util.List;
 
 import org.junit.jupiter.api.Test;
@@ -12,11 +13,17 @@ import com.karthik.jpm.marsrover.exception.CommandException;
 public class CommandTest {
     @Test
     public void should_throwException_if_CommandPattern_doesnt_match() {
+        List<String> patterns = new ArrayList<String>() {
+            {
+                add("Q");
+                add("W");
+            }
+        };
         Throwable throwable = assertThrows(CommandException.class,
-                () -> Command.validation.accept(null, List.of("Q", "W")));
+                () -> Command.validation.accept(null, patterns));
         assertEquals(throwable.getMessage(), "Not a valid command!");
         throwable = assertThrows(CommandException.class,
-                () -> Command.validation.accept("1,2", List.of("Q", "W")));
+                () -> Command.validation.accept("1,2", patterns));
         assertEquals(throwable.getMessage(), "Not a valid command!");
     }
 }
